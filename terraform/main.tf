893ae2c292b8fa0de2f9b4be108984c6abc21ab2
Gayathri <chinna888123@gmail.com>
2026-05-29 13:08:33 +0530

    Configured remote terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "cloud-app-terraform-state-992382534186"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
